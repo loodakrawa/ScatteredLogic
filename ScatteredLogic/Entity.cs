f8faebff6fb72a1470efceae65ada3502970d7d8
loodakrawa <loodakrawa@gmail.com>
2017-10-11 13:13:14 +1100

    Improve bits in Entity

--- ScatteredLogic/Entity.cs
+++ ScatteredLogic/Entity.cs
@@ -10,31 +10,25 @@ namespace ScatteredLogic
     public struct Entity : IEquatable<Entity>
     {
         internal const int IndexBits = 20;
-        internal const int IndexMask = 0xFFFFF;
+        internal const int IndexMask = (1 << IndexBits) - 1;
 
         private readonly int Id;
+
+        internal Entity(int id) => Id = id;
 
         public int Index => Id & IndexMask;
         public int Version => Id >> IndexBits;
 
         public bool Equals(Entity other) => Id == other.Id;
-        public override bool Equals(object obj) => obj is Entity ? Equals((Entity)obj) : false;
-        public override int GetHashCode() => Id;
 
         public static bool operator ==(Entity a, Entity b) => a.Id == b.Id;
         public static bool operator !=(Entity a, Entity b) => a.Id != b.Id;
 
-#if DEBUG
-        internal Entity(int id)
-        {
-            Id = id;
-            Name = null;
-        }
-        public string Name { get; set; }
-        public override string ToString() => string.Format("{0} ({1}:{2})", Name, Index, Version);
-#else
-        internal Entity(int id) => Id = id;
-#endif
+        public override bool Equals(object obj) => obj is Entity ? Equals((Entity)obj) : false;
+        public override int GetHashCode() => Id;
 
+#if DEBUG
+        public override string ToString() => string.Format("({0}:{1})", Index, Version);
+#endif
     }
 }
